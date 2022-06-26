4fd448abfa1a571cd1dc5cfae649ab43280a6709
give654 <give654@naver.com>
2022-06-27 02:39:46 +0900

    Basic_00000
    Added onEnable, onDisable messages.

--- src/main/java/com/erimus/steveparty/SteveParty.java
+++ src/main/java/com/erimus/steveparty/SteveParty.java
@@ -7,11 +7,12 @@ public final class SteveParty extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Steve Party PlugIn Activated!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Steve Party Plugin Deactivated!");
     }
 }
